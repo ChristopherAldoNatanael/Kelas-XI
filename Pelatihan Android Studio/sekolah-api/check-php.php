43a5374cfeef8fff518db47c1f75a98639acdfca
ChristopherAldoNatanael <christophernatanael123@gmail.com>
2025-12-03 12:25:55 +0700

    first commit

--- Pelatihan Android Studio/sekolah-api/check-php.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "PHP Version: " . phpversion() . "\n";
-echo "Memory Limit: " . ini_get('memory_limit') . "\n";
-echo "Max Execution Time: " . ini_get('max_execution_time') . "\n";
-echo "Error Reporting: " . ini_get('error_reporting') . "\n";
-phpinfo();
-?>
